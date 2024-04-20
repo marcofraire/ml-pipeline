eedc9a8c502ec491dc0a211edca2d01baa67ef65
Marco <frairemarco@gmail.com>
2024-04-20 09:50:11 +0100

    added ebay listing

--- config_load_db.py
+++ config_load_db.py
@@ -10,4 +10,25 @@ load_db_configurations = {
             "label": ("correct", "bool")
         }
     },
+    "ebay_listings": {
+        "url": {
+            "production": "https://bbourse-f97c610fe272.herokuapp.com/ebay_listings",
+            "development": "http://localhost:8000/ebay_listings"
+        },
+        "payload_template": {
+            "ebay_link": ("viewItemURL", "str"),
+            "edition_id": ("edition_id", "int"),
+            "img_link": ("galleryURL", "str"),
+            "title": ("title", "str"),
+            "price": ("price", "float"),
+            "listing_type": ("listing_type", "str"),
+            "seller": ("seller", "str"),
+            "seller_rating": ("seller_rating", "int"),
+            "seller_sales": ("seller_sales", "int"),
+            "country": ("country", "str"),
+            "signed": ("signed", "str"),   
+            "condition": ("condition", "str"),       
+            "sub_edition": ("sub_edition", "str"),               
+        }
+    },
 }
