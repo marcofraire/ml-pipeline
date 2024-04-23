e6dc67855479091b77081cb136e59f2234ba5bb3
Marco <frairemarco@gmail.com>
2024-04-23 18:39:23 +0100

    config added sales

--- config_load_db.py
+++ config_load_db.py
@@ -23,4 +23,24 @@ load_db_configurations = {
             "sub_edition": ("sub_edition", "str"),               
         }
     },
+    "ebay_sales": {
+        "url": {
+            "production": "https://bbourse-f97c610fe272.herokuapp.com/ebay_sales",
+            "development": "http://localhost:8000/ebay_sales"
+        },
+        "payload_template": {
+            "edition_id": ("edition_id", "int"),
+            "img_link": ("galleryURL", "str"),
+            "price": ("price", "float"),
+            "reduced": ("reduced", "int"),
+            "seller": ("seller", "str"),
+            "seller_rating": ("seller_rating", "float"),
+            "seller_sales": ("seller_sales", "float"),
+            "sale_data": ("sale_data", "date"),
+            "country": ("country", "str"),
+            "signed": ("signed", "str"),   
+            "condition": ("condition", "str"),       
+            "sub_edition": ("sub_edition", "str"),               
+        }
+    },
 }
