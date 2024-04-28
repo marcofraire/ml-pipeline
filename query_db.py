b1fd0b14db2052c6190c149e7b865f164940059b
Marco <frairemarco@gmail.com>
2024-04-28 13:37:40 +0100

    added queries

--- query_db.py
+++ query_db.py
@@ -34,3 +34,21 @@ class QueryMLBooks(Query):
 
     def compose_query(self):
         return f"SELECT * FROM bbourse.ml_books WHERE 1=1 and edition_id = {self.edition_id}"
+
+class QueryEbaySalesLinks(Query):
+
+    def __init__(self, db_details: dict, edition_id: str):
+        super().__init__(db_details)
+        self.edition_id = edition_id
+
+    def compose_query(self):
+        return f"SELECT img_link as img_link_db  FROM bbourse.ebay_sales WHERE 1=1 and edition_id = {self.edition_id}"
+
+class QueryEbayListings(Query):
+
+    def __init__(self, db_details: dict, edition_id: str):
+        super().__init__(db_details)
+        self.edition_id = edition_id
+
+    def compose_query(self):
+        return f"SELECT * FROM bbourse.ebay_listings WHERE 1=1 and edition_id = {self.edition_id}"
