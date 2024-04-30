f3ec5014f5e4087d1a80c3c0ae117dd115ee7f6b
Marco <frairemarco@gmail.com>
2024-04-30 16:21:47 +0100

    f

--- config_ml.py
+++ config_ml.py
@@ -2,7 +2,7 @@ ML_MODELS_DIRECTORY = '/content/drive/My Drive/Tech/Production/ML_Models/'
 PREDICTION_FILES_DIRECTORY = '/content/drive/My Drive/Tech/Production/Edition_Prediction_Files/New/'
 ARCHIVE_FILES_DIRECTORY = '/content/drive/My Drive/Tech/Production/Edition_Prediction_Files/Archive/'
 
-ML_MODELS_DIRECTORY = '/content/drive/My Drive/Tech/Model Training/'
+ML_MODELS_DIRECTORY = '/content/drive/My Drive/Tech/Model Training/Edition Recognition/'
 
 config_edition_ml = {
     '1': {
--- train_ml_model.py
+++ train_ml_model.py
@@ -6,6 +6,6 @@ from utils import get_timestamp
 def pull_ebay_images(app_id, edition_id, total_entries = 1000):
     df = EbayAPI(app_id, config_edition_ml[edition_id]['ebay_api_keywords'], total_entries).ebay_listings()
     df = df[['img_id','galleryURL', 'title','viewItemURL']]
-    df.to_excel(f'{ML_MODELS_DIRECTORY}/{edition_id}/Images/ebay_images_{get_timestamp()}.xlsx', index=False)
+    df.to_excel(f'{ML_MODELS_DIRECTORY}/{edition_id}Images/ebay_images_{get_timestamp()}.xlsx', index=False)
     return df
 
