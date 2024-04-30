66fd5ed8beccd9b473f821b84342634fc9a4313d
Marco <frairemarco@gmail.com>
2024-04-30 16:19:46 +0100

    f

--- train_ml_model.py
+++ train_ml_model.py
@@ -3,8 +3,8 @@ from config_ml import ML_MODELS_DIRECTORY, config_edition_ml
 from utils import get_timestamp
 
 
-def pull_ebay_images(app_id, edition_id, total_entries = 2000):
-    df = EbayAPI(app_id, config_edition_ml[edition_id]['ebay_api_keywords'], total_entries).get_output()
+def pull_ebay_images(app_id, edition_id, total_entries = 1000):
+    df = EbayAPI(app_id, config_edition_ml[edition_id]['ebay_api_keywords'], total_entries).ebay_listings()
     df = df[['img_id','galleryURL', 'title','viewItemURL']]
     df.to_excel(f'{ML_MODELS_DIRECTORY}/{edition_id}/Images/ebay_images_{get_timestamp()}.xlsx', index=False)
     return df
